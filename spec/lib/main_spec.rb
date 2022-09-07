d9ace133afc7c2ef32294abb4bf19e78f2c6ba19
immprada <im.mprada@gmail.com>
2022-09-07 11:24:49 -0500

    Add specs for days_filter

--- spec/lib/main_spec.rb
+++ spec/lib/main_spec.rb
@@ -12,4 +12,20 @@ describe 'lib/main' do
       stock_picker(prices).should == [1, 4]
     end
   end
+
+  describe 'days_filter' do
+    let(:residual_prices) { [6, 9, 15, 8, 6, 1, 10] }
+    let(:buy_day) { 1 }
+    let(:buy_price) { 3 }
+    let(:max_profit) { 0 }
+    let(:buy_sell) { [] }
+
+    it 'returns an array of size 2' do
+      days_filter(buy_day, buy_price, max_profit, residual_prices, buy_sell).size.should == 2
+    end
+
+    it 'returns [profit, [day_sell, day_buy(relative to day_sell)]]' do
+      days_filter(buy_day, buy_price, max_profit, residual_prices, buy_sell).should == [12, [1, 3]]
+    end
+  end
 end
